83f6c374ab36749de4dadd145874db73c146bc35
Andres Ricardo Perez <riperezro@unal.edu.co>
2022-09-20 18:06:43 -0500

    Online Gameplay

--- Talleres/Reto3/AndroidTicTacToe/app/src/main/java/co/edu/unal/androidtictactoe_tutorial2/OnlineGameActivity.java
+++ Talleres/Reto3/AndroidTicTacToe/app/src/main/java/co/edu/unal/androidtictactoe_tutorial2/OnlineGameActivity.java
@@ -21,8 +21,10 @@ import android.widget.TextView;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.sql.Timestamp;
 import java.util.Map;
@@ -72,20 +74,23 @@ public class OnlineGameActivity extends AppCompatActivity {
         mBoardView.setGame(mGame);
         // Listen for touches on the board
         mBoardView.setOnTouchListener(mTouchListener);
-        checkForWinnerAndUpdatePrompts();
         if( getIntent().getStringExtra(EXTRA_MESSAGE).equals("creator") ){
             mySpaces = BoardState.PLAYER_1;
             mGameCode = "" + System.currentTimeMillis();
             mGameCodeTextView.setText("Game Code: " + mGameCode);
             boardState = new BoardState(mGameCode);
+            mInfoTextView.setText(R.string.turn_human);
             writeNewBoard(boardState);
         }else{
             mySpaces = BoardState.PLAYER_2;
             mGameCode = getIntent().getStringExtra(GAME_CODE);
             mGameCodeTextView.setText("Game Code: " + mGameCode);
             isHumanTurn = false;
+            mInfoTextView.setText(R.string.turn_opponent);
+            boardState = new BoardState(mGameCode);
             loadNewBoard();
         }
+        setUpListeners();
     }
 
     @Override
@@ -144,6 +149,7 @@ public class OnlineGameActivity extends AppCompatActivity {
             int location = row * 3 + col;
             if (!mGameOver && isHumanTurn && setMove(TicTacToeGame.HUMAN_PLAYER, location)) {
                 setMove(TicTacToeGame.HUMAN_PLAYER, location);
+                isHumanTurn = false;
                 // If no winner yet, let the computer make a move
                 int winner = mGame.checkForWinner();
                 if(winner != 0) checkForWinnerAndUpdatePrompts();
@@ -156,16 +162,14 @@ public class OnlineGameActivity extends AppCompatActivity {
 
     private void checkForWinnerAndUpdatePrompts(){
         int winner = mGame.checkForWinner();
-        if (winner == 0)
-            mInfoTextView.setText(R.string.turn_human);
-        else if (winner == 1) {
+        if (winner == 1) {
             mInfoTextView.setText(R.string.result_tie);
             mGameOver = true;
         } else if (winner == 2) {
             mInfoTextView.setText(R.string.result_human_wins);
             mGameOver = true;
         } else {
-            mInfoTextView.setText(R.string.result_opponent_wins);
+            mInfoTextView.setText(R.string.result_rival_wins);
             mGameOver = true;
         }
     }
@@ -176,6 +180,12 @@ public class OnlineGameActivity extends AppCompatActivity {
             // Play the sound effect
             if(player == TicTacToeGame.HUMAN_PLAYER) mHumanMediaPlayer.start();
             else mComputerMediaPlayer.start();
+
+            //Update board for the other player.
+            if( player == TicTacToeGame.HUMAN_PLAYER ){
+                boardState.mBoard = updateStringAt(boardState.mBoard,mySpaces,location);
+                writeNewBoard(boardState);
+            }
             return true;
         }
         return false;
@@ -208,14 +218,52 @@ public class OnlineGameActivity extends AppCompatActivity {
                 }
                 else {
                     Map<String,Object> returnedValue = (Map) task.getResult().getValue();
-                    BoardState boardState = new BoardState(mGameCode,
+                    boardState = new BoardState(mGameCode,
                             (String) returnedValue.get("mBoard"));
 
                     mGame.setBoardState(boardStateToLocalBoard(boardState.mBoard));
                     mBoardView.invalidate();
+                    for( int i = 0 ; i < BoardState.BOARD_SIZE ; i ++ ){
+                        if( boardState.mBoard.charAt(i) != BoardState.OPEN_SPOT ){
+                            isHumanTurn = true;
+                            mInfoTextView.setText(R.string.turn_human);
+                        }
+                    }
                 }
             }
         });
+    }
+
+    private void setUpListeners() {
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.w("DB_READ", "UpdateBoard");
+                if(mGameOver || isHumanTurn) return;
+                BoardState newBoardState = dataSnapshot.child("boards").child(mGameCode).
+                        getValue(BoardState.class);
+                for( int i = 0 ; i < BoardState.BOARD_SIZE ; i ++ ){
+                    if( newBoardState.mBoard.charAt(i) != boardState.mBoard.charAt(i) ){
+                        boardState.mBoard = updateStringAt(boardState.mBoard,
+                                newBoardState.mBoard.charAt(i) ,i);
+                        setMove(TicTacToeGame.COMPUTER_PLAYER,i);
+                        int winner = mGame.checkForWinner();
+                        if(winner != 0) checkForWinnerAndUpdatePrompts();
+                        else {
+                            isHumanTurn = true;
+                            mInfoTextView.setText(R.string.turn_human);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("DB_ERROR", "UpdateBoard:Failed", databaseError.toException());
+            }
+        };
+        myRef.addValueEventListener(postListener);
     }
 
     private char[] boardStateToLocalBoard( String boardState ){
@@ -229,6 +277,12 @@ public class OnlineGameActivity extends AppCompatActivity {
                 ret = ret + TicTacToeGame.COMPUTER_PLAYER;
         }
         return ret.toCharArray();
+    }
+
+    private String updateStringAt( String s, char c,  int position ){
+        StringBuilder tempString = new StringBuilder(boardState.mBoard);
+        tempString.setCharAt(position, c);
+        return tempString.toString();
     }
 
 }
